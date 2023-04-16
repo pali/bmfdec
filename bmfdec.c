8b46f6ea8e903a11dd8276f018d7ec0eb0ba8e38
Armin Wolf <W_Armin@gmx.de>
2023-04-16 02:58:55 +0200

    bmfdec: Fix handling of short reads
    The functions read() and write() can experience a "short read/write",
    which requires issuing another read/write. The bmfdec utility does
    not handle this correctly, causing random failures due to "short reads/writes".
    Fix this by using the standard C functions fread()/fwrite(), which do
    handle "short reads/writes".
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- bmfdec.c
+++ bmfdec.c
@@ -18,9 +18,6 @@
 
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <unistd.h>
 
 #define INLINE static inline
 
@@ -259,8 +256,13 @@ int ds_dec(void* pin,int lin, void* pout, int lout, int flg)
  */
 
 static int process_data(char *data, uint32_t size) {
-  ssize_t ret = write(1, data, size);
-  return (ret > 0 && ret == size) ? 0 : 1;
+  size_t ret;
+
+  ret = fwrite(data, sizeof(*data), size, stdout);
+  if (ret != size)
+	  return 1;
+
+  return 0;
 }
 
 #undef process_data
@@ -269,16 +271,19 @@ static int process_data(char *data, uint32_t size);
 int main() {
   static uint32_t pin[0x10000/4];
   static char pout[0x40000];
-  ssize_t lin;
+  size_t lin;
   int lout;
-  lin = read(0, pin, sizeof(pin));
-  if (lin < 0) {
-    fprintf(stderr, "Failed to read data: %s\n", strerror(errno));
-    return 1;
-  } else if (lin == sizeof(pin)) {
-    fprintf(stderr, "Failed to read data: %s\n", strerror(EFBIG));
+  lin = fread(pin, sizeof(char), sizeof(pin), stdin);
+  if (ferror(stdin)) {
+    fprintf(stderr, "Failed to read data\n");
     return 1;
   }
+
+  if (!feof(stdin)) {
+    fprintf(stderr, "Data too large\n");
+    return 1;
+  }
+
   if (lin <= 16 || pin[0] != 0x424D4F46 || pin[1] != 0x01 || pin[2] != (uint32_t)lin-16 || pin[3] > sizeof(pout)) {
     fprintf(stderr, "Invalid input\n");
     return 1;
